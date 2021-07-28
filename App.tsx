7749da4c4b24e0ace3798725147e8d3166df32a5
murilo-souza <murilo.l.souza232@gmail.com>
2021-07-28 19:33:28 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#585655',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    color: '#fff'
+  }
 });
