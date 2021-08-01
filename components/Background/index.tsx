3f1c9293768d24a56ab81671f979abd4eedbb57c
murilo-souza <murilo.l.souza232@gmail.com>
2021-07-31 23:12:41 -0300

    update

---
+++ components/Background/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+  View
+} from 'react-native';
+
+import { styles } from './styles';
+
+
+export function Background(){
+  return (
+    <View>
+
+    </View>
+  );
+}
