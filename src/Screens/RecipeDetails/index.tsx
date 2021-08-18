e5ee8f4c7d2c699e29d4dddc791d925efd7ac626
murilo-souza <murilo.l.souza232@gmail.com>
2021-08-17 23:26:18 -0300

    repair recipeDetail screen

--- src/Screens/RecipeDetails/index.tsx
+++ src/Screens/RecipeDetails/index.tsx
@@ -18,59 +18,7 @@ export function RecipeDetails(){
           titleHeader="Detalhes da receita"
         />
         <View style={styles.container}>
-          <SafeAreaView>
-            <View style={styles.section}>
-                <Text style={styles.title}>Ingredientes</Text>
-                <FlatList
-                nestedScrollEnabled={true}
-                  data={[
-                    {key: 'Feijão preto'},
-                    {key: 'Calabresa'},
-                    {key: 'Bacon'},
-                    {key: 'Carne de Sol'},
-                    {key: 'Laranja'},
-                    {key: 'Joelho de porco'},
-                    {key: 'Panceta'},
-                    {key: 'Couve'}
-                  ]}
-                  renderItem={({item})=> <Text style={styles.text}>{item.key}</Text>}
-                />
-              </View>
-              <View style={styles.section}>
-              <Text style={styles.title}>Modo de preparo</Text>
-              <FlatList
-              nestedScrollEnabled={true}
-                  data={[
-                    {key: '1. Faça o feijão em fogo baixo'},
-                    {key: '2. Frite a calabresa e o bacon'},
-                    {key: '3. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '4. Coloque laranja para suavizar o sabor'},
-                    {key: '5. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '6. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '7. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '8. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '9. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '10. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '11. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '12. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '13. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '13. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '13. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '13. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '13. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '13. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '13. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '13. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '13. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '13. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '13. Dessalgue a carne seca e depois ha faça na panela'},
-                    {key: '13. Dessalgue a carne seca e depois ha faça na panela'},
-                      
-                  ]}
-                  renderItem={({item})=> <Text style={styles.text}>{item.key}</Text>}
-                />
-              </View>
-            </SafeAreaView>
+          
         </View>
     </View>
   );
